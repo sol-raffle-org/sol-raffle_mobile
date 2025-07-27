ea243f84c21ac0ca6e10e99c53ef4a7d7587006c
hieu.chuvan <hieu0311@gmail.com>
2025-07-27 13:46:16 +0700

    Revert file app/_layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,7 @@
 import { AppProviders } from '@/components/app-providers'
 import { AppSplashController } from '@/components/app-splash-controller'
-import { KEY_TOKEN } from '@/constants/app.const'
+import { useAuth } from '@/components/auth/auth-provider'
 import { useTrackLocations } from '@/hooks/use-track-locations'
-import useAppStore from '@/stores/useAppStore'
-import { AccountInterface } from '@/types/app.type'
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
@@ -55,12 +52,7 @@ export default function RootLayout() {
 }
 
 function RootNavigator() {
-  // const { isAuthenticated } = useAuth()
-  const { setAccountInfo } = useAppStore()
-
-  const isAuthenticated = true
-  AsyncStorage.setItem(KEY_TOKEN, verifyObject.accessToken)
-  setAccountInfo(verifyObject.user)
+  const { isAuthenticated } = useAuth()
 
   return (
     <Stack
@@ -69,36 +61,13 @@ function RootNavigator() {
       }}
     >
       <Stack.Protected guard={isAuthenticated}>
-        <Stack.Screen name="profile" />
         <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="+not-found" />
       </Stack.Protected>
 
       <Stack.Protected guard={!isAuthenticated}>
         <Stack.Screen name="connect-wallet" />
       </Stack.Protected>
-
-      <Stack.Screen name="+not-found" />
     </Stack>
   )
-}
-
-const verifyObject: { user: AccountInterface; [x: string]: any } = {
-  user: {
-    createdAt: '2025-07-18T01:54:48.139Z',
-    id: '5b286458-e1a3-426e-8641-7a9066ab4e66',
-    wallet: 'nWzLy6CfTTR19dXHQw4ka9QCS3YbEWbTSmtSkjJYqNA',
-    name: 'Harris_1234',
-    email: 'test@gmail.com',
-    avatar: '1752820410602-541253806.jpg',
-    referralCode: null,
-    referralByWallet: null,
-    level: 6,
-    exp: 2850,
-    language: 'en',
-    isEmailVerified: false,
-    nextLevelExp: 4050,
-  },
-  accessToken:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViMjg2NDU4LWUxYTMtNDI2ZS04NjQxLTdhOTA2NmFiNGU2NiIsIndhbGxldEFkZHJlc3MiOiJuV3pMeTZDZlRUUjE5ZFhIUXc0a2E5UUNTM1liRVdiVFNtdFNrakpZcU5BIiwiaWF0IjoxNzUzNTkzMTE0LCJleHAiOjE3NTQxOTc5MTR9.glVHXPvJHXadoOMVBo-8637Rrw7452UUKsWoiskTtms',
-  expirationAt: 1754197914369,
 }
