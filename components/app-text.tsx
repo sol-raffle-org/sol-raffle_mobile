998877a65b257f5e7eedf5bde53af6b71d9583a3
hieu.chuvan <hieu0311@gmail.com>
2025-07-31 09:33:54 +0700

    UI: Add font

--- components/app-text.tsx
+++ components/app-text.tsx
@@ -1,6 +1,6 @@
-import { Text, TextProps } from 'react-native-paper'
 import { Text as NativeText } from 'react-native'
+import { Text, TextProps } from 'react-native-paper'
 
-export function AppText({ ...rest }: TextProps<NativeText>) {
-  return <Text {...rest} />
+export function AppText({ style, ...otherProps }: TextProps<NativeText>) {
+  return <Text style={[{ fontFamily: 'InriaSans-Regular' }, style]} {...otherProps} />
 }
