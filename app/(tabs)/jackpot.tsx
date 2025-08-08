ef7f5b3ced800b057350d40b6ee585b8ccbb2f36
Andrew <minhtung112@gmail.com>
2025-08-09 01:17:00 +0700

    feat: update animation jp

--- app/(tabs)/jackpot.tsx
+++ app/(tabs)/jackpot.tsx
@@ -1,17 +1,24 @@
 import { JackpotBackgroundImage } from '@/assets/images'
+import { AppImage } from '@/components/app-image'
 import { AutoHeightImage } from '@/components/app-image-height'
 import { AppPage } from '@/components/app-page'
 import AppTabView from '@/components/app-tab-view'
 import { JackpotPlayInRound } from '@/components/jackpot/jackpot-play-in-round'
 import { JackpotSlotMachineAction } from '@/components/jackpot/jackpot-slot-machine-action'
+import { useCountdownByTimestamp } from '@/hooks/common/useCountdown'
 import useJackpotStore from '@/stores/useJackpotStore'
-import { useEffect, useState } from 'react'
+import { JackpotGameStatus } from '@/types/jackpot.type'
+import { isNil } from '@/utils/common.utils'
+import { WEBVIEW_JACKPOT_URL } from '@env'
+import { useEffect, useMemo, useState } from 'react'
 import { ScrollView, View } from 'react-native'
 import { WebView } from 'react-native-webview'
-import { WEBVIEW_JACKPOT_URL } from '@env'
 
 export default function JackpotScreen() {
   const { jackpotGameData } = useJackpotStore()
+  const remainTime = useCountdownByTimestamp(
+    !jackpotGameData?.endTime || jackpotGameData?.endTime <= 0 ? 0 : Math.floor(jackpotGameData?.endTime / 1000),
+  )
 
   const [random, setRandom] = useState(0)
 
@@ -21,10 +28,15 @@ export default function JackpotScreen() {
     setRandom(Math.random())
   }, [jackpotGameData])
 
+  const isShowWinning = useMemo(() => {
+    if (!jackpotGameData || isNil(remainTime)) return false
+
+    if (jackpotGameData.status === JackpotGameStatus.Awarding && remainTime > 0 && remainTime <= 6) return true
+  }, [jackpotGameData, remainTime])
+
   return (
     <AppTabView>
       <AutoHeightImage source={JackpotBackgroundImage} style={{ position: 'absolute', top: 0, left: 0, right: 0 }} />
-
       <ScrollView
         showsVerticalScrollIndicator={false}
         style={{ flex: 1 }}
@@ -62,6 +74,19 @@ export default function JackpotScreen() {
             }}
           />
         </View>
+
+        {isShowWinning && (
+          <View style={{ width: '100%', height: 520, position: 'absolute', top: 0, zIndex: 50 }}>
+            <AppImage
+              source={require('@/assets/lottie/lottie-raining-money.gif')}
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            />
+          </View>
+        )}
 
         <AppPage style={{ flexDirection: 'column', gap: 8, marginTop: -30 }}>
           <JackpotSlotMachineAction />
