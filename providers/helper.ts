ed326e1f9d8059e051a89d16a5923135ccf9c76e
Andrew <minhtung112@gmail.com>
2025-08-09 23:46:01 +0700

    feat: update logic create  game

--- providers/helper.ts
+++ providers/helper.ts
@@ -1,46 +1,39 @@
-import { FlipGameInterface, FlipGameStatusEnum } from "@/types/coin-flip.type";
+import { FlipGameInterface, FlipGameStatusEnum } from '@/types/coin-flip.type'
 
-export const updateFlipGameData = (
-  flipGameData?: FlipGameInterface[],
-  updatedData?: any,
-  kick?: boolean
-) => {
-  if (!flipGameData?.length || !updatedData) return;
+export const updateFlipGameData = (flipGameData?: FlipGameInterface[], updatedData?: any, kick?: boolean) => {
+  if (!flipGameData?.length && updatedData) {
+    return [updatedData]
+  }
+  if (!flipGameData?.length || !updatedData) return
 
-  if (!flipGameData?.length || !updatedData) return;
   if (updatedData.status === FlipGameStatusEnum.Created && !kick) {
-    const temp = [...flipGameData];
-    const currentTime = Math.floor(Date.now() / 1000);
-    const firstItemEnd = temp.findIndex(item => {
-      return (
-        Number(item.deleteTime || 0) > 0 &&
-        currentTime >= Math.floor((item?.deleteTime || 0) / 1000)
-      );
-    });
+    const temp = [...flipGameData]
+    const currentTime = Math.floor(Date.now() / 1000)
+    const firstItemEnd = temp.findIndex((item) => {
+      return Number(item.deleteTime || 0) > 0 && currentTime >= Math.floor((item?.deleteTime || 0) / 1000)
+    })
     if (firstItemEnd !== -1) {
-      temp[firstItemEnd] = updatedData;
+      temp[firstItemEnd] = updatedData
     } else {
-      temp.push(updatedData);
+      temp.push(updatedData)
     }
 
-    return temp;
+    return temp
   }
 
-  const newGameData = flipGameData.map(item => {
+  const newGameData = flipGameData.map((item) => {
     const matched =
-      item.userCreator.wallet ===
-        (updatedData.creator || updatedData?.userCreator?.wallet) &&
-      (item.gameId === updatedData.gameId || item.seed === updatedData?.seed);
+      item.userCreator.wallet === (updatedData.creator || updatedData?.userCreator?.wallet) &&
+      (item.gameId === updatedData.gameId || item.seed === updatedData?.seed)
 
     return matched
       ? {
           ...item,
           ...updatedData,
-          userJoin:
-            kick === true ? null : updatedData?.userJoin || item.userJoin,
+          userJoin: kick === true ? null : updatedData?.userJoin || item.userJoin,
         }
-      : item;
-  }) as FlipGameInterface[];
+      : item
+  }) as FlipGameInterface[]
 
-  return newGameData as FlipGameInterface[];
-};
+  return newGameData as FlipGameInterface[]
+}
